fe72180caaa8536d4d738b1bad65c6d288aab73e
Gustavo Valenzuela <GALTdea@gmail.com>
2022-05-10 15:14:58 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+   root "cities#index"
+  resources :cities
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
